02d4f496fba005f1cb39dcaf506159f7f71c652d
Nataniel Freire de Paiva <nataniel.paiva@gmail.com>
2015-08-25 22:34:32 -0300

    subindo

---
+++ projeto2/module/Aluno/src/Aluno/Model/Aluno.php
@@ -0,0 +1,40 @@
+<?php
+namespace Aluno\Model;
+
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+class Aluno {
+
+    public $id;
+
+    public $nome;
+
+    public $idade;
+    
+    protected $inputFilter;
+
+    public function exchangeArray($data)
+    {
+        $this->id = (isset($data['id'])) ? $data['id'] : null;
+        $this->nome = (isset($data['nome'])) ? $data['nome'] : null;
+        $this->idade = (isset($data['idade'])) ? $data['idade'] : null;
+    }
+
+    public function getInputFilter()
+    {
+       
+    }
+
+
+    public function getArrayCopy()
+    {
+        return array(
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'idade' => $this->idade
+        );
+    }
+}
